f652a51e06459f33407cca4387290a57a7dbcaa2
Barani203 <naveenkumarmy203@gmail.com>
2022-03-13 13:21:24 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import BusinessIcon from "@material-ui/icons/Business";
 import SubjectIcon from "@material-ui/icons/Subject";
 import ReactQuill from "react-quill";
 //ES6
-import "react-quill/dist/quill.snow.css"; 
+import "react-quill/dist/quill.snow.css";
 import React, { useState } from "react";
 import axios from "axios";
 import "./style.css";
-import Alert from '@mui/material/Alert';
-
+import Alert from "@mui/material/Alert";
 
 function App() {
   const Form = () => {
@@ -46,16 +45,14 @@ function App() {
           console.log({ email, message, subject, company });
 
           let payload = {
-
             email: tags,
             subject: subject,
             message: message,
             company: company,
-           
           };
 
           axios({
-            url: "http://localhost:3030/mail",
+            url: "https://bulkmailtool2.herokuapp.com/",
             method: "post",
             data: payload,
           })
@@ -78,132 +75,120 @@ function App() {
     };
     return (
       <>
-      {/* tag input */}
-        <div class="container">
-          <div className="row m-3">
-            <div className="col-sm-4 mt-3 ">
-              <div className="tag-input">
-                <ul className="tags">
-                  {tags.map((tag, index) => {
-                    return (
-                      <li key={index} className="tag">
-                        <span className="tag-title">{tag}</span>
-                        <span
-                          className="tag-close-icon"
-                          onClick={() => removeTag(tag)}
-                        >
-                          x
-                        </span>
-                      </li>
-                    );
-                  })}
-                </ul>
-                <input
-                  id="email"
-                  type="email"
-                  onKeyDown={addTag}
-                  placeholder="Press space to add a Email"
-                />
-              </div>
-            </div>
-            <div className="col-sm-8  ">
+        {/* tag input */}
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 mr-5 ">
               {/* form field */}
-            <form onSubmit={handleRequest} method="post">
-              <div className="form">
-                <div className="form__wrapper">
-                  <div className="form__title">
-                    <h4>{loading ? "Sending..." : "Bulk Email"}</h4>
-                    {loading && (
-                     
-                     
-                     <Alert variant="filled" severity="success">
-                      Email Sent Successfully check it out!
-                   </Alert>
-                      
-                    )}
-              
-                  </div>
-                  <div className="form__container">
-                    <div className="form__containerItems">
-                      <div className="form__containerItem"></div>
-                      <div className="form__containerItem">
-                        <div className="form__containerItemName">
-                          <label>Email</label>
+              <form onSubmit={handleRequest} method="post">
+                <div className="form">
+                  <div className="form__wrapper">
+                    <div className="form__title">
+                      <h4>{loading ? "Sending..." : "Bulk Email"}</h4>
+                      {loading && (
+                        <Alert variant="filled" severity="success">
+                          Email Sent Successfully check it out!
+                        </Alert>
+                      )}
+                    </div>
+                    <div className="form__container">
+                      <div className="form__containerItems">
+                        <div className="form__containerItem"></div>
+                        <div className="form__containerItem">
+                          <div className="form__containerItemName">
+                            <label>Email</label>
 
-                          <EmailIcon />
+                            <EmailIcon />
+                          </div>
+                          <div className="form__containerItemField">
+                            <div className="tag-input">
+                              <ul className="tags">
+                                {tags.map((tag, index) => {
+                                  return (
+                                    <li key={index} className="tag">
+                                      <span className="tag-title">{tag}</span>
+                                      <span
+                                        className="tag-close-icon"
+                                        onClick={() => removeTag(tag)}
+                                      >
+                                        x
+                                      </span>
+                                    </li>
+                                  );
+                                })}
+                              </ul>
+                              <input
+                                id="email"
+                                value={email}
+                                type="email"
+                                onKeyDown={addTag}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required={true}
+                                placeholder="Press space to add a Email"
+                              />
+                            </div>
+                          </div>
                         </div>
-                        <div className="form__containerItemField">
-                          <input
-                            id="email"
-                            onKeyDown={addTag}
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required={true}
-                            type="text"
-                            placeholder="Press space to add a Email"
+                        <div className="form__containerItem">
+                          <div className="form__containerItemName">
+                            <label>Company</label>
+                            <BusinessIcon />
+                          </div>
+                          <div className="form__containerItemField">
+                            <input
+                              id="company"
+                              value={company}
+                              onChange={(e) => setCompany(e.target.value)}
+                              type="text"
+                              placeholder="Enter Your Company Name"
+                            />
+                          </div>
+                        </div>
+                        <div className="form__containerItem">
+                          <div className="form__containerItemName">
+                            <label>Subject</label>
+                            <SubjectIcon />
+                          </div>
+                          <div className="form__containerItemField">
+                            <input
+                              id="subject"
+                              value={subject}
+                              onChange={(e) => setSubject(e.target.value)}
+                              required
+                              type="text"
+                              placeholder="Add Subject"
+                            />
+                          </div>
+                        </div>
+                        <div className="form__containerItem">
+                          <div className="form__containerItemName">
+                            <label>Compose Mail</label>
+                          </div>
+                        </div>
+                        <div className="container__composeMail">
+                          <ReactQuill
+                            id="message"
+                            value={message}
+                            onChange={handleQuillChange}
+                            className="quill"
+                            placeholder="Enter Content from here..."
                           />
                         </div>
-                      </div>
-                      <div className="form__containerItem">
-                        <div className="form__containerItemName">
-                          <label>Company</label>
-                          <BusinessIcon />
+                        <div className="send">
+                          <button
+                            disabled={loading}
+                            onClick={handleRequest}
+                            type="submit"
+                            className="btn btn-success"
+                          >
+                            Send
+                          </button>
                         </div>
-                        <div className="form__containerItemField">
-                          <input
-                            id="company"
-                            value={company}
-                            onChange={(e) => setCompany(e.target.value)}
-                            type="text"
-                            placeholder="Enter Your Company Name"
-                          />
-                        </div>
-                      </div>
-                      <div className="form__containerItem">
-                        <div className="form__containerItemName">
-                          <label>Subject</label>
-                          <SubjectIcon />
-                        </div>
-                        <div className="form__containerItemField">
-                          <input
-                            id="subject"
-                            value={subject}
-                            onChange={(e) => setSubject(e.target.value)}
-                            required
-                            type="text"
-                            placeholder="Add Subject"
-                          />
-                        </div>
-                      </div>
-                      <div className="form__containerItem">
-                        <div className="form__containerItemName">
-                          <label>Compose Mail</label>
-                        </div>
-                      </div>
-                      <div className="container__composeMail">
-                        <ReactQuill
-                          id="message"
-                          value={message}
-                          onChange={handleQuillChange}
-                          className="quill"
-                          placeholder="Enter Content from here..."
-                        />
-                      </div>
-                      <div className="send">
-                        <button
-                          disabled={loading}
-                          onClick={handleRequest}
-                          type="submit"
-                          className="btn btn-success"
-                        >
-                          Send
-                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
             </div>
           </div>
         </div>
